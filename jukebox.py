d8a34dc6c97ebd4d2a841676955129adba589f53
Ali Raheem <ali.raheem@gmail.com>
2012-03-16 01:30:27 +0000

    Better database

--- jukebox.py
+++ jukebox.py
@@ -1,27 +1,15 @@
 #!/usr/bin/python
 import serial, os, sys, sqlite3
 
-def play(code):
-	try:
-		c.execute('select cmd from tags where tag=?',(code,))
-	except sqlite3.OperationalError:
-		c.execute('create table tags (tag text, cmd text)')
-		play(code)
-	result = c.fetchone()
-	if(result):
-		print result[0]
-		os.system(result[0]+"&")
-	else:
-		print "Tag not found!"
-		if(raw_input("Would you like to add it? [y/n]")!='y'):
-			return 1
-		cmd = raw_input()
+s = serial.Serial("/dev/ttyACM0", 115200) # Change me to the device
+dbName = "db"
+db = sqlite3.connect(dbName)
+c = db.cursor()
+s.open()
+lastTag = 0
 
-		c.execute("insert into tags values (?, ?)", (code, cmd, ))
-		print "Added",code,"to run '",cmd,"'"
-		db.commit()
-		return 0
 def parse(code):
+#Should return a plain string used as the SQL id.
 	code = code.split('-')[1]
 	try:
 		code = code.split('\r')[0]
@@ -32,20 +20,41 @@ def parse(code):
 			print "Probs not a code."
 	return code
 
-db_name = "db"
-db = sqlite3.connect(db_name)
-c = db.cursor()
+def play(code):
+	global lastTag
+	try:
+		c.execute('select cmd,name from tags where tag=?',(code,))
+	except sqlite3.OperationalError:
+		c.execute('create table tags (id INTEGER PRIMARY KEY, tag TEXT UNIQUE, name TEXT, cmd TEXT)')
+		play(code)
+	result = c.fetchone()
+	if(result):
+		cmd = result[0]
+		name = result[1]
+		lastTag = code
+		print "Loading %s..."%name
+		os.system(cmd+"&")
+	else:
+		print "Tag not found!"
+		if(raw_input("Would you like to add it? [y/n]")!='y'):
+			return 1
+		name = raw_input("Give it a name: ")
+		cmd = raw_input("What command should be run: ")
+		c.execute("insert into tags values (NULL, ?, ?, ?)", (code, name, cmd, ))
+		print "Added %s (%s) to run '%s'"%(name, code, cmd)
+		db.commit()
+		return 0
 
-s = serial.Serial("/dev/ttyACM0", 115200)
-s.open()
 def main():
 	while 1:
 		if(s.inWaiting()):
 			code =  s.read(s.inWaiting())
-			print "Read code",code,"..."
 			code = parse(code)
-			play(code)
-
+			print "Read code %s..."%code
+			if(code != lastTag):
+				print code,lastTag
+				play(code)
+			s.flushOutput()
 if __name__ == "__main__":
         sys.exit(main())
 
