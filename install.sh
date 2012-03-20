d2458d5c231798b6ec01660318893c1b6f12ec31
Ali Raheem <ali.raheem@gmail.com>
2012-03-20 22:41:27 +0000

    Added rudimentary (un)install script

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir /etc/jukebox
+cp jukebox /usr/bin/jukebox
+cp edit_db /usr/sbin/jukebox_edit_db
---
+++ uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -rf /etc/jukebox
+rm /usr/bin/jukebox
+rm /usr/sbin/jukebox_edit_db
